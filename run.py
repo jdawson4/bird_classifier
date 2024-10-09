7bbf960639585f0d8ca7ac5b094345bfbf2ce158
Jacob Dawson <jacob.dawson@me.com>
2024-10-09 19:51:17 -0400

    format, add README

--- run.py
+++ run.py
@@ -2,17 +2,17 @@
 
 from architecture import *
 
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
+physical_devices = tf.config.experimental.list_physical_devices("GPU")
 num_gpus = len(physical_devices)
 print(f"Number of GPUs available: {num_gpus}")
 
 train_imgs = keras.utils.image_dataset_from_directory(
     "train_imgs/",
-    labels = None,
-    color_mode = 'rgb',
-    batch_size = batch_size,
-    image_size = (image_size, image_size),
+    labels=None,
+    color_mode="rgb",
+    batch_size=batch_size,
+    image_size=(image_size, image_size),
     shuffle=True,
-    interpolation='bilinear',
-    seed = seed
+    interpolation="bilinear",
+    seed=seed,
 )
